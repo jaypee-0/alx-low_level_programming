4e08d206bf64bd52fe887a917c3aa40918390286
Jaypee <eyibrajohnpaul@gmail.com>
2022-07-09 09:38:49 +0100

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ *
+ * Description - prints number 0 through 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
