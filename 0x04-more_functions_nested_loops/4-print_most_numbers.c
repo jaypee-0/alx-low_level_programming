392796e6ad7705afaaee83aca55dc6790f974d9c
Jaypee <eyibrajohnpaul@gmail.com>
2022-07-09 09:41:22 +0100

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers
+ *
+ * Description - prints number 0 through 9 except for 2 and 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
+
