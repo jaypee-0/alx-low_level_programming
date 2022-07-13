772b95799709d1530fd567c7216cdfa34b29d012
Jaypee <eyibrajohnpaul@gmail.com>
2022-07-13 15:59:38 +0100

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets a variable to 98
+ * @n: address to a variable in memory
+ *
+ * Description: sets an the address of n to 98
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
