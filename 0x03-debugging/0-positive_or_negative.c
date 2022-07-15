a0e8c0c2cd235462be5ceb60885abfa4c2152f07
Jaypee <eyibrajohnpaul@gmail.com>
2022-07-15 22:53:28 +0100

    main

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - sign checking
+ *
+ * Description: checks whether the reandom number
+ * generate is positive or negative
+ *
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	} else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	} else
+	{
+		printf("%d is negative\n", i);
+	}
+}
